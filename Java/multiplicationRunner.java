8c512afe560c5301660e2b4a6092865cecb14dac
Vivek Suresh <vivekslair@gmail.com>
2021-02-22 18:31:34 +0530

    Add files via upload

---
+++ Java/multiplicationRunner.java
@@ -0,0 +1,10 @@
+package com.exercises;
+
+public class multiplicationRunner {
+
+	public static void main(String[] args) {
+		multiplicationTable func = new multiplicationTable();
+		func.multiplicationfunc(5,17,24);
+		}
+	}
+	
---
+++ Java/multiplicationTable.java
@@ -0,0 +1,13 @@
+package com.exercises;
+
+public class multiplicationTable {
+	
+	void multiplicationfunc(int i,int from , int to) {
+		for (int j=from;j<=to;j++) {
+			System.out.printf("%d * %d = %d", i,j,i*j).println();
+		}
+	}
+			
+}
+
+
